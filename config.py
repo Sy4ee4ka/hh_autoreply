9cd6f4d8e1ea307363d1f60b90468d9023d75738
Sy4ee4ka <i.kokuev2002@gmail.com>
2026-03-12 02:41:09 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -4,19 +4,19 @@
 
 # ── API Key ────────────────────────────────────────────────────
 # Get yours free at: https://aistudio.google.com/app/apikey
-GEMINI_API_KEY = "AIzaSyC7kWKuj32ol_4ynouHDdGqePxFN8F6aQ0"
+GEMINI_API_KEY = "You api key"
 
 # ── Your Personal Info ─────────────────────────────────────────
-YOUR_NAME     = "Кокуев Илья Германович"          # e.g. "Alex Johnson"
-YOUR_POSITION = "Системный аналитик"    # e.g. "Senior Python Developer"
-YOUR_SKILLS = "SQL (PostgreSQL, MS SQL Server), REST API, SOAP, JSON, XML, Postman, Swagger, BPMN, UML, IDEF0, Системный анализ, Бизнес-анализ, Постановка задач разработчикам (User Stories, Use Cases), Apache Kafka, Docker, Git"
+YOUR_NAME     = "Full name"          # e.g. "Alex Johnson"
+YOUR_POSITION = "Full position"    # e.g. "Senior Python Developer"
+YOUR_SKILLS = Full skills"	#e.g. "SQL (PostgreSQL, MS SQL Server), REST API, SOAP, JSON, XML, Postman, Swagger, BPMN, UML, IDEF0"
 
 # Optional: education, certifications, languages, portfolio link, etc.
-EXTRA_INFO = "education: Moscrow RTU MIREA"
+EXTRA_INFO = "any info"
 
 # ── Letter Settings ────────────────────────────────────────────
 # Language for the generated cover letter: "English", "Russian", etc.
-LETTER_LANGUAGE = "Russian"
+LETTER_LANGUAGE = "English"
 
 # Tone: "professional", "friendly", "formal"
 LETTER_TONE = "professional"
